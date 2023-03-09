a35a8667cdee222c205a6c98230b42b49019e911
Anwar Louis <smmaniac@hotmail.co.uk>
2023-03-09 17:42:25 +0000

    Show current results on main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { getScores } from './api';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [gameArray, setGameArray] = useState([]);
+  const [csvContent, setCsvContent] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const tmpScores = await getScores();
+      console.log(tmpScores);
+      setCsvContent(tmpScores);
+    }
+    fetchData();
+  }, []);
+
+  return <pre>{csvContent}</pre>;
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
